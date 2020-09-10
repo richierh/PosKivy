889cba805daa1d309487e3026df2769aea9e1eca
richie <pmc.computer@yahoo.com>
2020-09-10 17:11:41 +0700

    Siap

--- views/screen_page.py
+++ views/screen_page.py
@@ -7,15 +7,14 @@ from kivymd.uix.list import OneLineIconListItem, MDList
 from kivy.properties import StringProperty, ListProperty
 from kivymd.theming import ThemableBehavior
 from kivy.uix.button import Button
+from kivymd.uix.navigationdrawer import MDNavigationDrawer
 
 kv_main = """
-
 ScreenManager:
     LoginScreen:
 
     PosScreen:
         name:"pos_m"
-
 
 """
 
@@ -50,21 +49,11 @@ kv_screen1 = """
 """
 kv_screen2 = """
 <PosScreen@Screen>:
-    BoxLayout:
-        orientation:"horizontal" 
-        Button2:
-        Button:
-            text: "Sign In"
-        Label:
-            text: "Sign Up"
-
+    Navi:
 """
-
-
 
 class ContentNavigationDrawer(BoxLayout):
     pass
-
 
 class ItemDrawer(OneLineIconListItem):
     icon = StringProperty()
@@ -99,29 +88,43 @@ class ScreenM(ScreenManager):
         s1.add_widget(PosScreen())
         s1.add_widget(LoginScreen())
 
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        '''Called when tap on a menu item.'''
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+    def on_start(self):
+            icons_item = {
+                "folder": "My files",
+                "account-multiple": "Shared with me",
+                "star": "Starred",
+                "history": "Recent",
+                "checkbox-marked": "Shared with me",
+                "upload": "Upload",
+            }
+            for icon_name in icons_item.keys():
+                self.root.ids.content_drawer.ids.md_list.add_widget(
+                    ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+                )
+
+
 class ScreenManagerApp(MDApp):
 
     def build(self):
-        Builder.load_file("views/coba.kv")
+        # Builder.load_file("views/coba.kv")
+        Builder.load_file("views/navi.kv")
         Builder.load_string(kv_screen1)
         Builder.load_string(kv_screen2)
         Window.size =  (400,600)
         self.title = "Point Of sales - Kivy"
         return Builder.load_string(kv_main)
-
-    # def on_start(self):
-    #         icons_item = {
-    #             "folder": "My files",
-    #             "account-multiple": "Shared with me",
-    #             "star": "Starred",
-    #             "history": "Recent",
-    #             "checkbox-marked": "Shared with me",
-    #             "upload": "Upload",
-    #         }
-    #         for icon_name in icons_item.keys():
-    #             self.root.ids.content_drawer.ids.md_list.add_widget(
-    #                 ItemDrawer(icon=icon_name, text=icons_item[icon_name])
-    #             )
 
 def main():
     ScreenManagerApp().run()
