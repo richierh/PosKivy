1ecfd0c5908207a93d181c961e71030189b39083
richierh <richie130283@gmail.com>
2022-03-09 07:34:08 +0700

    okay

---
+++ py/main_menu.py
@@ -0,0 +1,9 @@
+from kivymd.uix.screen import MDScreen
+
+
+class MainMenu(MDScreen):
+
+
+    def back_mainmenu(self):
+        import pdb
+        pdb.set_trace()
---
+++ py/register_screen.py
@@ -0,0 +1,4 @@
+from kivymd.uix.screen import MDScreen
+
+class RegisterScreen(MDScreen):
+    pass
